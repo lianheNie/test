212b1ee061558612c18b03f99cccc720bdb8e8a6
lianhe Nie <1370920830@qq.com>
2021-09-03 17:11:54 +0800

    快速排序

--- 源.cpp
+++ 源.cpp
@@ -3,20 +3,20 @@
 #include<string.h> 
 #include <math.h>
 #define INPUT 0xA6
-char* GetMemory(void)
-{
-    char p[] = "hello world";
-    return p;
-}
-unsigned char bit_reverse(unsigned char input) {
-	unsigned char res = 0;
-	int bit = 8;
-	while (bit--) {
-		res |= ((input & 0x01) << bit);
-		input >>= 1;
-	}
-	return res;
-}
+//char* GetMemory(void)
+//{
+//    char p[] = "hello world";
+//    return p;
+//}
+//unsigned char bit_reverse(unsigned char input) {
+//	unsigned char res = 0;
+//	int bit = 8;
+//	while (bit--) {
+//		res |= ((input & 0x01) << bit);
+//		input >>= 1;
+//	}
+//	return res;
+//}
 
 //找出一个字符串中一个最长的连续数字，并标注出位置和长度
 //char* find(char* a, int* size) {
@@ -80,10 +80,75 @@ unsigned char bit_reverse(unsigned char input) {
 //	}
 //	return result;
 //}
-float fff = 1.234;
-char buf[128];
-int main(void)
+//int main()
+//{
+//    int a[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//    memcpy(a + 3, a, 9);
+//    for (int i = 0; i < 10; i++)
+//    {
+//        printf("%d", a[i]);
+//    }
+//}
+//void count() {
+//    int x, y, z, number;
+//    number = 0;
+//    for (x = 0; x <= 100; x++) {
+//        for (y = 0; y <= 50; y++)
+//        {
+//            if (100 - x - y * 2 >= 0 && (100 - x - y * 2) % 5 == 0) {
+//                number++;
+//            }
+//            
+//        }
+//    }
+//    printf("%d\t", number);
+//}
+//
+//int do_dup(int a[], int N)
+//{
+//    int temp = 0;
+//    while (a[0] != a[a[0]])
+//    {
+//        temp = a[0];
+//        a[0] = a[temp];
+//        a[temp] = temp;
+//    }
+//    return a[0];
+//    
+//}
+
+void quick_sort(int* num, int start_num, int end_num)
 {
-    sprintf(buf, "%.3f\n\r", fff);//A
-    while (1);
+    if (start_num < end_num)
+    {
+        int i = start_num;
+        int j = end_num;
+        int temp = num[start_num];  // 以第一个元素为基准  
+        while (i < j)
+        {
+            while (i < j && num[j] > temp)  // 从后往前寻找比基准小的元素  
+                j--;
+            if (i < j)
+                num[i++] = num[j];      // 插在基准元素前面的空位上  
+            while (i < j && num[i] <= temp) // 从前往后找比基准大的元素  
+                i++;
+            if (i < j)
+                num[j--] = num[i];       // 插在基准元素后面的空位上  
+        }
+        num[i] = temp;                 // 基准元素归位  
+        quick_sort(num, start_num, i - 1);  //递归地对基准元素左边的数据排序  
+        quick_sort(num, i + 1, end_num);   //递归地对基准元素右边的数据排序  
+    }
+}
+
+int main()
+{
+    int a[5] = {2,1,3,4,3};
+    int i = 0;
+    quick_sort(a, 0, 4);
+    for (i; i < 5; i++)
+    {
+        printf("%d", a[i]);
+    }
+    return 0;
 }
